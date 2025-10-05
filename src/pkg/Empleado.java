91155d650024ede938ab1bea7e940fd4bdc7c226
aleja <aleja@Ale>
2025-10-05 17:33:53 +0200

    Empleado

--- src/pkg/Empleado.java
+++ src/pkg/Empleado.java
@@ -8,7 +8,6 @@ public class Empleado {
 	float Extras = 0;
 	
 	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtras) {
-		
 		if(tipo==TipoEmpleado.Vendedor) {
 			salarioBase=2000;
 		}else if(tipo==TipoEmpleado.Encargado) {
@@ -24,7 +23,18 @@ public class Empleado {
 		Extras = horasExtras*30;
 		
 		return salarioBase+primas+Extras;
+	}
+	
+	public float calculoNominaNeta(float nominaBruta) {
+		float res = nominaBruta;
 		
+		if(nominaBruta>2100 && nominaBruta<2500 ) {
+			return (float) (res*1.15);
+		}else if(nominaBruta>=2500) {
+			return (float) (res*1.18);
+		} 
+		
+		return res;
 		
 	}
 }
